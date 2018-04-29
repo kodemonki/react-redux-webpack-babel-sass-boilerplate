7c2bd0e003c60aef49e2764a3dbfb389236b0124
Tom Jane <kodemonki@hotmail.com>
2018-04-29 17:47:13 +0100

    connected redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware
+} from "redux";
+import {
+  Provider
+} from "react-redux";
+import {
+  createLogger
+} from "redux-logger";
 
 import App from "./js/components/App";
-
 import './styles/appStyles.scss';
 
-ReactDOM.render( <App/>, document.getElementById("App"));
+const initialState = {
+  initialised: "initialised"
+};
+
+const reducer = (state = initialState, action) => {
+  var newState = Object.assign({}, state);
+  switch (action.type) {
+    case "CONSOLE_LOG":
+      console.log("CONSOLE_LOG "+action.payload);
+      break;
+  }
+  return newState;
+}
+
+const store = createStore(
+  combineReducers({
+    reducer
+  }), {},
+  applyMiddleware(createLogger())
+);
+
+store.dispatch({
+  type: "CONSOLE_LOG",
+  payload: "Redux dispatch"
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider> , document.getElementById("App"));
--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,12 +1,35 @@
 import React from "react";
+import {connect} from "react-redux";
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
+    this.props.consoleLog('React dispatch');
+
     return <Router>
       <div className="App">
-        <h1>react-webpack-babel-sass-boilerplate</h1>
+        <h1>react-redux-webpack-babel-sass-boilerplate</h1>
+        <p>Props from Redux : {this.props.initialised}</p>
       </div>
     </Router>;
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    initialised: state.reducer.initialised
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    consoleLog: (message) => {
+      dispatch({
+        type: "CONSOLE_LOG",
+        payload: message
+      })
+    }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
